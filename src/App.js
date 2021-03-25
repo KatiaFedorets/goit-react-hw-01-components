d31781e7c78b062a8c803fad89c040970dccdf21
Katerina Fedorets <katia.fedorets@gmail.com>
2021-03-25 21:29:08 +0200

    fix all

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import 'normalize.css';
-import Profile from './components/Profile/Profile';
+import Profile from './components/FriendList/FriendList';
 import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import user from './Profile/user.json';
-import statisticalData from './Statistics/statistical-data.json';
-import friends from './FriendList/friends.json';
-import transactions from './TransactionHistory/transactions.json'
+import user from './components/Profile/user.json';
+import statisticalData from './components/Statistics/statistical-data.json';
+import friends from './components/FriendList/friends.json';
+import transactions from './components/TransactionHistory/transactions.json'
 
 function App() {
   return (
