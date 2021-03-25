8d1f59d367c54ec1154df6050b50bff02e2b7c84
Katerina Fedorets <katia.fedorets@gmail.com>
2021-03-25 21:20:45 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import 'normalize.css';
-import Profile from '.components/Profile/Profile';
-import Statistics from '.components/Statistics/Statistics';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
-import TransactionHistory from '.components/TransactionHistory/TransactionHistory';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import user from './Profile/user.json';
 import statisticalData from './Statistics/statistical-data.json';
 import friends from './FriendList/friends.json';
@@ -10,21 +10,21 @@ import transactions from './TransactionHistory/transactions.json'
 
 function App() {
   return (
-  <div>
-    <Profile
-      name={user.name}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-      />
-    <Statistics
-      title="Upload stats"
-      stats={statisticalData}
-      />
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+        />
+      <Statistics
+        title="Upload stats"
+        stats={statisticalData}
+        />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions}/>
-  </div>
+    </div>
     
   );
 }
